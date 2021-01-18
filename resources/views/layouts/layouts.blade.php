e364f1c65d4dbac105aea5b87f5cbe7ccf34a122
EC2 Default User <ec2-user@ip-172-31-14-58.us-east-2.compute.internal>
2021-01-18 13:14:14 +0000

    herokuへデプロイ

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -19,6 +19,6 @@
         @component('components.footer')
         @endcomponent
         
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
