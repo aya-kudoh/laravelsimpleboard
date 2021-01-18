a4272b2ad97adda2f40d9b11ad53720f57376d91
EC2 Default User <ec2-user@ip-172-31-14-58.us-east-2.compute.internal>
2021-01-18 10:58:29 +0000

    viewport設定

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -2,6 +2,7 @@
     <head>
         <title>@yield('title')</title>
         <meta name="csrf_token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, inital-scale=1.0">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
