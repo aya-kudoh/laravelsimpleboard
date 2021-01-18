7dd4145f70d9c733c03bc49d2cc36af5fa25d528
EC2 Default User <ec2-user@ip-172-31-14-58.us-east-2.compute.internal>
2021-01-18 03:13:43 +0000

    ヘッダー・フッダーの実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,26 @@
-<h1>Posts</h1>
+@extends('layouts.layouts')
 
-@foreach($posts as $post)
-    <a heaf="/posts/{{ $post-id }}">{{ $post-title }}</a>
-    <a heaf="/posts/{{ $posts-id }}/edit">Edit</a>
+@section('tiele', 'Simple Board')
+
+@section('content')
+
+  @if (session('message'))
+      {{ session('message') }}
+  @endif
+
+  <h1>Posts</h1>
+
+  @foreach($posts as $post)
+      <a heaf="/posts/{{ $post->id }}">{{ $post->title }}</a>
+      <a heaf="/posts/{{ $post->id }}/edit">Edit</a>
     
-    <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete Are you sure?')) { return true } else { return false };">
-        <input type="hidden" name="_method" value="DELETE">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <button type="submit">Delete</button>
-    </form>
-@endforeach
+      <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else { return false };">
+          <input type="hidden" name="_method" value="DELETE">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <button type="submit">Delete</button>
+      </form>
+  @endforeach
 
 
-<a href="/posts/create">New Post</a>
+  <a href="/posts/create">New Post</a>
+@endsection
